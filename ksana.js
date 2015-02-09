3dc9fead4bbf1d9f7a10a4f3fba2d2523cc77266
ch2550592000 <ch2550592000@yahoo.com.tw>
2015-02-09 15:43:04 +0800

    firstbuild

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 5,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://ch2550592000@rawgit.com/ch2550592000/myapp/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  552,
+  64,
+  157633,
+  277980,
+  645243,
+  738,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-02-09T07:28:04.224Z",
+  "2015-02-09T07:28:04.210Z",
+  "2015-02-09T07:40:52.983Z",
+  "2015-02-09T07:28:04.240Z",
+  "2015-02-09T07:28:04.272Z",
+  "2015-02-09T07:38:00.119Z",
+  "2015-02-09T07:28:04.195Z",
+  "2015-02-09T07:28:04.182Z",
+  "2015-02-09T07:40:37.876Z"
+ ],
+ "date": "2015-02-09T07:40:53.934Z"
+})
